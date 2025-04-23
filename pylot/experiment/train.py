11197330c1b4ad15ad00f829789ec0a49fffab64
EtienneChollet <etiennechollet2626@gmail.com>
2025-04-23 10:27:00 -0400

    [DOCS] Add documentation to train.py

--- pylot/experiment/train.py
+++ pylot/experiment/train.py
@@ -1,9 +1,19 @@
+"""
+Class for training experiments.
+
+Example
+-------
+>>> from universeg.experiment import TrainExperiment
+>>> exp =  TrainExperiment.from_config(config)
+>>> exp.run()
+"""
+
 import copy
 import pathlib
-import sys
 from typing import List
 
 import torch
+import wandb
 from torch import nn
 from torch.utils.data import DataLoader
 
@@ -16,10 +26,64 @@ from .util import absolute_import, eval_config
 
 
 class TrainExperiment(BaseExperiment):
+    """
+    A training experiment to be subclassed for concrete implementations.
+
+    Attributes
+    ----------
+    config : dict
+        inherited from `BaseExperiment`.
+    properties : dict
+        inherited from `BaseExperiment`.
+    metadata : dict
+        inherited from `BaseExperiment`.
+    train_dataset : torch.utils.data.Dataset
+        Training dataset set by `build_data()` method.
+    val_dataset : torch.utils.data.Dataset
+        Training dataset set by `build_data()` method.
+    train_dl : torch.utils.data.DataLoader
+        Dataloader for training data, set by `build_dataloader()` method.
+    val_dl : torch.utils.data.DataLoader
+        Dataloader for training data, set by `build_dataloader()` method.
+    model : torch.nn.Module
+        Model with trainable parameters
+    properties : dict
+        Properties and metadata of `TrainExperiment`
+    optim : torch.optim
+    """
+
     def __init__(self, path):
+        """
+        Initialize experiment.
+
+        Parameters
+        ----------
+        path : str
+            Path to the directory containing a `config.yml`
+
+        Notes
+        -----
+        This method initializes the components of an experiment in the
+        following order:
+        1. Build the datasets (`train_dataset` and `val_dataset` attrs)
+        2. Build the dataloaders (`train_dl` and `val_dl` attrs)
+        3. Initialize the optimizer (`optim` attr)
+        4. Build the metrics (`metric_fns`)
+        5. Build the augmentations (`augmentations` only in concrete instances)
+        """
+
+        # Enable cuDNN auto-tuner for performance
         torch.backends.cudnn.benchmark = True
+
+        # Initialize parent `BaseExperiment`
         super().__init__(path)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Set cuda if available
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+        # Build data, model, optimizer, loss, metrics, augmentations
         self.build_data()
         self.build_model()
         self.build_optim()
@@ -28,50 +92,128 @@ class TrainExperiment(BaseExperiment):
         self.build_augmentations()
 
     def build_data(self):
+        """
+        Set `train_dataset` and `val_dataset` instance attributes.
+
+        Reads dataset configuration from the `data` field of `self.config`
+        and initializes datasets for training and validation splits.
+        """
+
+        # Make sure config file has 'data' field
+        assert 'data' in self.config.keys(), (
+            f'"data" key not found in {self.path}/config.yml'
+        )
+
+        # Get the dataset configruations
         data_cfg = self.config["data"].to_dict()
-        dataset_constructor = data_cfg.pop("_class", None) or data_cfg.pop("_fn")
+
+        # Get the class or function constructor for the dataset
+        dataset_constructor = data_cfg.pop(
+            "_class", None
+        ) or data_cfg.pop("_fn")
+
+        # Dynamically import the constructor
         dataset_cls = absolute_import(dataset_constructor)
 
+        # Set the train and val dataset attrs by making an instance of the
+        # dataset classes and feeding in kwargs
         self.train_dataset = dataset_cls(split="train", **data_cfg)
         self.val_dataset = dataset_cls(split="val", **data_cfg)
 
     def build_dataloader(self):
+        """
+        Set `train_dl` and `val_dl` instance attributes. 
+
+        Reads dataloader configuration from the `dataloader` field of
+        `self.config`
+        """
+
+        # Make sure config file has 'dataloader' field
+        assert 'dataloader' in self.config.keys(), (
+            f'"dataloader" key not found in {self.path}/config.yml'
+        )
+
+        # Get the dataloader configuraion
+        dl_cfg = self.config["dataloader"]
+
+        # Make sure the batch size is less than the total training set
         assert self.config["dataloader.batch_size"] <= len(
             self.train_dataset
-        ), "Batch size larger than dataset"
-        dl_cfg = self.config["dataloader"]
+        ), (
+        f'Batch size = {self.config["dataloader.batch_size"]} should not be '
+        f'larger than dataset (len = {len(self.train_dataset)})'
+        )
+
+        # Set the validation dataloader attribute
         self.train_dl = DataLoader(
             self.train_dataset, shuffle=True, drop_last=False, **dl_cfg
         )
+
+        # Set the validation dataloader attribute
         self.val_dl = DataLoader(
             self.val_dataset, shuffle=False, drop_last=False, **dl_cfg
         )
 
     def build_model(self):
+        """
+        Initialize the model from config and record param count.
+        """
+
+        # Set the model
         self.model = eval_config(self.config["model"])
+
+        # Compute and store the number of trainable parameters into properties
         self.properties["num_params"] = num_params(self.model)
 
     def build_optim(self):
-        optim_cfg = self.config["optim"].to_dict()
-        # TODO add lr_scheduler support
-        # self.lr_scheduler = eval_config(opt_cfg.pop('lr_scheduler', None)
+        """
+        Initialize optimizer from config, handling weight decay.
+        """
 
+        # Get the optimizer dict from config
+        optim_cfg = self.config["optim"].to_dict()
+
+        # Optionaly handle weight decay for param groups
         if "weight_decay" in optim_cfg:
             optim_cfg["params"] = split_param_groups_by_weight_decay(
                 self.model, optim_cfg["weight_decay"]
             )
+    
         else:
             optim_cfg["params"] = self.model.parameters()
 
+        # Set the `optim` instance attr
         self.optim = eval_config(optim_cfg)
 
     def build_loss(self):
+        """
+        Initialize the loss function from configuration.
+
+        Reads the `loss_func` field of `self.config` and sets the
+        `loss_func` attribute.
+        """
+
+        # Instantiate loss function
         self.loss_func = eval_config(self.config["loss_func"])
 
     def build_metrics(self):
+        """
+        Initialize metric functions for logging.
+
+        Reads the `log.metrics` field of `self.config` (if present)
+        and sets `metric_fns` to a dict of callables.
+        """
+
+        # Initialize container for storing metrics
         self.metric_fns = {}
+
         if "log.metrics" in self.config:
-            self.metric_fns = eval_config(copy.deepcopy(self.config["log.metrics"]))
+
+            # Deep copy to avoid unwanted side effects
+            metrics_config = copy.deepcopy(self.config["log.metrics"])
+
+            # Initialize the metric functions
+            self.metric_fns = eval_config(metrics_config)
 
     def build_initialization(self):
         if "initialization" in self.config:
@@ -155,10 +297,12 @@ class TrainExperiment(BaseExperiment):
         self.build_callbacks()
 
         last_epoch: int = self.properties.get("epoch", -1)
+
         if last_epoch >= 0:
             self.load(tag="last")
             df = self.metrics.df
             autosave(df[df.epoch < last_epoch], self.path / "metrics.jsonl")
+
         else:
             self.build_initialization()
 
@@ -168,29 +312,25 @@ class TrainExperiment(BaseExperiment):
         checkpoint_freq: int = self.config.get("log.checkpoint_freq", 1)
         eval_freq: int = self.config.get("train.eval_freq", 1)
 
-        # self.run_callbacks("setup")
-
-        # try:
-
         for epoch in range(last_epoch + 1, epochs):
+
             print(f"Start epoch {epoch}")
+
             self._epoch = epoch
             self.run_phase("train", epoch)
+
             if eval_freq > 0 and (epoch % eval_freq == 0 or epoch == epochs - 1):
                 self.run_phase("val", epoch)
 
             if checkpoint_freq > 0 and epoch % checkpoint_freq == 0:
                 self.checkpoint()
+
             self.run_callbacks("epoch", epoch=epoch)
 
         self.checkpoint(tag="last")
 
         self.run_callbacks("wrapup")
 
-        # except KeyboardInterrupt:
-        #     print(f"Interrupted at epoch {epoch}. Tearing Down")
-        #     self.checkpoint(tag="interrupt")
-        #     sys.exit(1)
 
     def run_phase(self, phase, epoch):
 
@@ -213,14 +353,30 @@ class TrainExperiment(BaseExperiment):
                     augmentation=augmentation,
                     epoch=epoch,
                 )
+
                 metrics = self.compute_metrics(outputs)
+
                 meters.update(metrics)
+
                 self.run_callbacks(
                     "batch", epoch=epoch, batch_idx=batch_idx, phase=phase
                 )
 
-        metrics = {"phase": phase, "epoch": epoch, **meters.collect("mean")}
+        meters_collect = meters.collect("mean")
+
+        metrics = {"phase": phase, "epoch": epoch, **meters_collect}
+
+        wandb_metrics = {
+            'epoch': epoch,
+            f'{phase}_loss': meters_collect['loss'],
+            f'{phase}_dice_score': meters_collect['dice_score'],
+            }
+
+        if self.config.get('wandb.track_it', False):
+            wandb.log(wandb_metrics, step=epoch)
+    
         self.metrics.log(metrics)
+
         return metrics
 
     def run_step(self, batch_idx, batch, backward=True, augmentation=True, epoch=None):
@@ -251,8 +407,3 @@ class TrainExperiment(BaseExperiment):
     def build_augmentations(self):
         pass
 
-
-# Example use:
-# >>> from universeg.experiment import TrainExperiment
-# >>> exp =  TrainExperiment.from_config(config)
-# >>> exp.run()
