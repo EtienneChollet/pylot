0a50e6fcde8b1f2d6732b4783d61ebe94e000f43
EtienneChollet <etiennechollet2626@gmail.com>
2025-06-03 20:15:03 -0400

    [ARCH] Add imports to __init__'s

--- pylot/experiment/__init__.py
+++ pylot/experiment/__init__.py
@@ -1,3 +1,11 @@
+from . import base
+from . import config
+from . import distributed
+from . import finetune
+from . import half
+from . import train
+from . import util
+
 from .base import BaseExperiment
 from .train import TrainExperiment
 from .util import autoload_experiment
